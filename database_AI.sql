22d651fa83496a23299e5092070cbbdfad12506d
OsmanK98 <osmankrzysztof123@gmail.com>
2020-12-19 14:35:57 +0100

    update database_AI.sql

--- database_AI.sql
+++ database_AI.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 18 Gru 2020, 23:17
+-- Czas generowania: 19 Gru 2020, 14:34
 -- Wersja serwera: 10.4.14-MariaDB
 -- Wersja PHP: 7.4.11
 
@@ -50,7 +50,19 @@ CREATE TABLE `contractors` (
 --
 
 INSERT INTO `contractors` (`id`, `name`, `vat_id`) VALUES
-(1, 'Osek Corporation', 'NIP123');
+(1, 'Osek Corporation', 'NIP123'),
+(3, 'test1', ''),
+(4, 'test2', ''),
+(5, 'test3', ''),
+(6, 'test4', ''),
+(7, 'test5', ''),
+(8, 'test6', ''),
+(9, 'test7', ''),
+(10, 'test8', 'NIP15213'),
+(11, 'Test9', ''),
+(12, 'TEST10', ''),
+(13, 'TEST11', ''),
+(14, 'TEST12', '');
 
 -- --------------------------------------------------------
 
@@ -110,7 +122,19 @@ CREATE TABLE `invoices` (
 --
 
 INSERT INTO `invoices` (`id`, `invoice_number`, `sum_netto`, `sum_vat`, `sum_brutto`, `date_of_invoice`, `dirpath`, `type`, `contractor_id`, `category_id`) VALUES
-(1, 'INV1142', '400.00', 24, '501.00', '2020-12-18', NULL, 'sale', 1, NULL);
+(1, 'INV1142', '400.00', 24, '501.00', '2020-12-18', NULL, 'sale', 1, NULL),
+(3, 'inv1', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(4, 'FAKTURA2', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(5, 'FAKTURA3', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(6, 'FAKTURA4', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(7, 'FAKTURA5', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(8, 'FAKTURA6', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(9, 'FAKTURA7', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(10, 'FAKTURA8', '0.00', 0, '0.00', '0000-00-00', NULL, '', 10, NULL),
+(11, 'INV9', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(12, 'INV10', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(13, 'INV11', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL),
+(14, 'INV12', '0.00', 0, '0.00', '0000-00-00', NULL, '', 3, NULL);
 
 -- --------------------------------------------------------
 
@@ -150,7 +174,19 @@ CREATE TABLE `licences` (
 --
 
 INSERT INTO `licences` (`id`, `sku`, `name`, `description`, `serial_number`, `buy_date`, `warranty_to`, `valid_to`, `price_netto`, `vat`, `price_brutto`, `who_uses`, `invoice_id`) VALUES
-(1, '1', 'Windows 10', 'Licencja do windowsa 10', 'serial123Key', '2020-12-18', '2020-12-18', '2020-12-18', '100.00', 23, '200.00', 'Cris', 1);
+(1, '1', 'Windows 10', 'Licencja do windowsa 10', 'serial123Key', '2020-12-18', '2020-12-18', '2020-12-18', '100.00', 23, '200.00', 'Cris', 1),
+(3, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 3),
+(4, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 4),
+(5, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 5),
+(6, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 6),
+(7, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 7),
+(8, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 8),
+(9, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 9),
+(10, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 10),
+(11, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 11),
+(12, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 12),
+(13, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 13),
+(14, '', '', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '0.00', 0, '0.00', '', 14);
 
 -- --------------------------------------------------------
 
@@ -250,7 +286,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT dla tabeli `contractors`
 --
 ALTER TABLE `contractors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT dla tabeli `documents`
@@ -268,13 +304,13 @@ ALTER TABLE `equipement`
 -- AUTO_INCREMENT dla tabeli `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT dla tabeli `licences`
 --
 ALTER TABLE `licences`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
