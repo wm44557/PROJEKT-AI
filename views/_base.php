5ae8bf545c3dc3657d5e4b0cf1ce552fccab1311
Patryk-Kowalczyk <tojestkowala@gmail.com>
2020-12-20 23:30:38 +0100

    Working on menu, added menu skeleton in user panel

--- views/_base.php
+++ views/_base.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
     <title>Fakturant</title>
 </head>
 <body>
-    <?php if(isset($_SESSION['user_id'])) :?>
-    <nav>Navbar <form action="<?php echo STARTING_URL?>/logout" method="post"><input type="submit" value="Wyloguj"></form> </nav>
-    <?php endif ?>
 
     <?php echo $content ?>
 
