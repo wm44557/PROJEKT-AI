c9b52e9a0536781e8f63dfbccb7de3f54053b39c
Patryk-Kowalczyk <tojestkowala@gmail.com>
2020-12-21 11:49:53 +0100

    Login page

--- views/_base.php
+++ views/_base.php
@@ -3,14 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="<?php echo "/" . STARTING_URL . "/css/style.css" ?>">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">
     <title>Fakturant</title>
 </head>
 <body>
+<?php dump(STARTING_HTTP) ?>
+<div class="background">
+        <div class="container">
+    <?php if (isset($_SESSION["user_role"]) && $_SESSION["user_role"] == "user"): ?>
+        <nav class="nav">
+            <div class="nav-logo">
+                <img src="./images/logo.png">
+            </div>
+            <ul class="nav-menu">
+                <a href="<?php echo STARTING_URL?>/user/add-invoice">
+                    <li class="nav-menu__item active">
+                        <i class="material-icons">library_add</i>
+                        <p>Dodaj fakturę</p>
+                    </li>
+                </a>
+                <li class="nav-menu__item">
+                    <i class="material-icons">history_edu</i>
+                    <p>Faktury</p>
+                </li>
+                <li class="nav-menu__item">
+                    <i class="material-icons">assignment</i>
+                    <p>Licencje</p>
+                </li>
+                <li class="nav-menu__item">
+                    <i class="material-icons">devices</i>
+                    <p>Sprzęty</p>
+                </li>
+                <li class="nav-menu__item">
+                    <i class="material-icons">settings</i>
+                    <p>Ustawienia użytkownika</p>
+                </li>
+            </ul>
+            <form  action="<?php echo STARTING_URL?>/logout" method="post"><input  class="button primary" type="submit" value="Wyloguj"></form>
+        </nav>
+            <div class="content">
+                <?php else: ?>
+                <div class="content" style="grid-column: span 2; width: 100%;">
+    <?php endif; ?>
+
 
     <?php echo $content ?>
+            </div>
 
+        </div>
+    </div>
 </body>
 </html>
--- views/pages/user/panel.php
+++ views/pages/user/panel.php
@@ -1,39 +1,11 @@
 
-<div class="background">
-    <div class="container">
-        <nav class="nav">
-            <div class="nav-logo">
-                <img src="./images/logo.png">
-            </div>
-            <ul class="nav-menu">
-                <li class="nav-menu__item">
-                    <i class="material-icons">library_add</i>
-                    <p>Dodaj fakturę</p>
-                </li>
-                <li class="nav-menu__item">
-                    <i class="material-icons">history_edu</i>
-                    <p>Faktury</p>
-                </li>
-                <li class="nav-menu__item">
-                    <i class="material-icons">assignment</i>
-                    <p>Licencje</p>
-                </li>
-                <li class="nav-menu__item">
-                    <i class="material-icons">devices</i>
-                    <p>Sprzęty</p>
-                </li>
-                <li class="nav-menu__item"></li>
-                <li class="nav-menu__item"></li>
-            </ul>
-            <form  action="<?php echo STARTING_URL?>/logout" method="post"><input  class="button primary" type="submit" value="Wyloguj"></form>
-        </nav>
-        <div class="content">
+
+
             <h1>Panel User</h1>
             <a href="<?php echo STARTING_URL?>/user/add-invoice"><button>Dodaj fakture</button></a><br><br>
             <a href="<?php echo STARTING_URL?>/user/list-invoice"><button>Wyswietl faktury</button></a>
-        </div>
-    </div>
-</div>
+
+
 
 
 
