b3e59b6f88b6a740c7a3ffd06bb0c561bf6dcdf6
Patryk-Kowalczyk <tojestkowala@gmail.com>
2021-01-12 12:14:45 +0100

    Full working invoice form

--- views/pages/components/invoiceForm.php
+++ views/pages/components/invoiceForm.php
@@ -2,7 +2,7 @@
 
 <a style="text-align: right" href="<?php echo STARTING_URL . '/' . $_SESSION['user_role']?>"><button class="button secondary small">Wróć</button></a>
 <br><br>
-<form class="form-box" method="post">
+<form class="form-box" method="post" enctype="multipart/form-data">
     <div class="form-info">
         <h2>Dane podstawowe:</h2>
         <p>Podstawowe dane dotyczące faktury.</p>
@@ -104,10 +104,87 @@
             </table>
         </div>
         <div class="buttons-group">
-            <p class="button warning small" onclick="deleteInvoiceTableFields();"><i class="material-icons">remove</i>Usuń</p>
-            <p class="button primary small" onclick="addNewInvoiceTableField();"><i class="material-icons">add</i>Dodaj</p>
+            <p class="button warning small" onclick="deleteLicenceTableFields();"><i class="material-icons">remove</i>Usuń</p>
+            <p class="button primary small" onclick="addNewLicenceTableField();"><i class="material-icons">add</i>Dodaj</p>
         </div>
         <input type="hidden" name="licencesCount" id="licencesCount" value="1">
+    </div>
+
+    <div class="form-table-container">
+        <h2>Sprzęty</h2>
+        <p>Wprowadź sprzęty przypisane do podanej faktury</p>
+        <br>
+        <div class="table-container">
+            <table class="form-table equipement">
+                <thead>
+                <tr>
+                    <th></th>
+                    <th>SKU</th>
+                    <th>Nazwa</th>
+                    <th>Opis</th>
+                    <th>Numer seryjny</th>
+                    <th>Data zakupu</th>
+                    <th>Gwarancja do</th>
+                    <th>Cena netto</th>
+                    <th>Vat</th>
+                    <th>Cena brutto</th>
+                    <th>Kto używa</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr id="equipement-1" data-order="1">
+                    <td>
+                        <input type="checkbox" class="equipement-checkbox" name="equipementDelete-1">
+                    </td>
+                    <td>
+                        <input type="text" name="equipementSku-1">
+                    </td>
+                    <td>
+                        <input type="text" name="equipementName-1">
+                    </td>
+                    <td>
+                        <input type="text" name="equipementDescription-1">
+                    </td>
+                    <td>
+                        <input type="text" name="equipementSerial-1">
+                    </td>
+                    <td>
+                        <input type="date" name="equipementBuyDate-1">
+                    </td>
+                    <td>
+                        <input type="date" name="equipementWarrantyDate-1">
+                    </td>
+                    <td>
+                        <input type="number" name="equipementNetto-1">
+                    </td>
+                    <td>
+                        <input type="number" name="equipementVat-1" min="0" max="100">
+                    </td>
+                    <td>
+                        <input type="number" name="equipementBrutto-1">
+                    </td>
+                    <td>
+                        <input type="text" name="equipementWhoUses-1">
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="buttons-group">
+            <p class="button warning small" onclick="deleteEquipementTableFields();"><i class="material-icons">remove</i>Usuń</p>
+            <p class="button primary small" onclick="addNewEquipementTableField();"><i class="material-icons">add</i>Dodaj</p>
+        </div>
+        <input type="hidden" name="equipementCount" id="equipementCount" value="1">
+    </div>
+
+    <div class="form-table-container">
+        <h2>Pliki</h2>
+        <p>Tutaj możesz dodać pliki powiązane z fakturą</p>
+        <br>
+        <input type="file" name="file">
+        <input class="form-input" type="text" placeholder="Opis" name="description">
+        <input type="hidden" name="invoiceId" value="<?php echo $invoiceId; ?>">
+        <br>
     </div>
 
     <div class="form-info">
@@ -138,7 +215,7 @@
 
 
 <script>
-    const INPUTS = [
+    const INPUTS_LICENCES = [
         {
             type: "checkbox",
             class: "licence-checkbox",
@@ -200,7 +277,65 @@
         },
     ];
 
-    function addNewInvoiceTableField(){
+    const INPUTS_EQUIPEMENT = [
+        {
+            type: "checkbox",
+            class: "equipement-checkbox",
+            name: "equipementDelete",
+        },
+        {
+            type: "text",
+            class: "",
+            name: "equipementSku",
+        },
+        {
+            type: "text",
+            class: "",
+            name: "equipementName",
+        },
+        {
+            type: "text",
+            class: "",
+            name: "equipementDescription",
+        },
+        {
+            type: "text",
+            class: "",
+            name: "equipementSerial",
+        },
+        {
+            type: "date",
+            class: "",
+            name: "equipementBuyDate",
+        },{
+            type: "date",
+            class: "",
+            name: "equipementWarrantyDate",
+        },
+        {
+            type: "number",
+            class: "",
+            name: "equipementNetto",
+        },
+        {
+            type: "number",
+            class: "",
+            name: "equipementVat",
+            additional: {"min": 0, "max": 100}
+        },
+        {
+            type: "number",
+            class: "",
+            name: "equipementBrutto",
+        },
+        {
+            type: "text",
+            class: "",
+            name: "equipementWhoUses",
+        },
+    ];
+
+    function addNewLicenceTableField(){
         const countElement = document.querySelector('#licencesCount');
         const count = parseInt(countElement.value) + 1;
         countElement.value = count;
@@ -210,7 +345,7 @@
         const rowEl = document.createElement('tr');
         rowEl.id = `licence-${count}`;
 
-        INPUTS.forEach( input => {
+        INPUTS_LICENCES.forEach( input => {
             const tdEl = document.createElement('td');
             const inputEl = document.createElement('input');
             inputEl.type = input.type;
@@ -236,8 +371,54 @@
 
     }
 
-    function deleteInvoiceTableFields(){
+    function deleteLicenceTableFields(){
         const checkboxes = document.querySelectorAll('.licence-checkbox');
+
+        const checkedCheckboxes = [...checkboxes].filter( checkbox => checkbox.checked );
+
+        checkedCheckboxes.map( checkbox => {
+            checkbox.parentElement.parentElement.remove();
+        })
+    }
+
+    function addNewEquipementTableField(){
+        const countElement = document.querySelector('#equipementCount');
+        const count = parseInt(countElement.value) + 1;
+        countElement.value = count;
+
+        const tableBody = document.querySelector('table.form-table.equipement tbody');
+
+        const rowEl = document.createElement('tr');
+        rowEl.id = `equipement-${count}`;
+
+        INPUTS_EQUIPEMENT.forEach( input => {
+            const tdEl = document.createElement('td');
+            const inputEl = document.createElement('input');
+            inputEl.type = input.type;
+            inputEl.name = `${input.name}-${count}`;
+            if (input.class){
+                inputEl.classList.add(input.class);
+            }
+            if (typeof input.additional !== "undefined"){
+                Object.entries(input.additional).forEach( entry => {
+                    const [key, value] = entry;
+                    if (key==="min"){
+                        inputEl.min = value;
+                    } else if(key==="max"){
+                        inputEl.max = value;
+                    }
+                })
+            }
+            tdEl.appendChild(inputEl);
+            rowEl.appendChild(tdEl);
+
+        });
+        tableBody.appendChild(rowEl);
+
+    }
+
+    function deleteEquipementTableFields(){
+        const checkboxes = document.querySelectorAll('.equipement-checkbox');
 
         const checkedCheckboxes = [...checkboxes].filter( checkbox => checkbox.checked );
 
